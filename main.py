68d4180e5588ca4b7fb35995cafdfe5e08766c8e
Louis <louismacconnell@hotmail.com>
2020-02-04 20:41:59 +1030

    connects to db and can find single word if spelt correctly

---
+++ main.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    user = "ardit700_student",
+    password = "ardit700_student",
+    host = "108.167.140.122",
+    database = "ardit700_pm1database"
+)
+cursor = connection.cursor()
+word = input("ENTER A WORD...\n")
+
+
+query = cursor.execute("SELECT * FROM Dictionary WHERE Expression= '%s'" % word)
+results = cursor.fetchall()
+for item in results:
+    print(item[1])
